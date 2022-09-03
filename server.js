2495cf319ec1b81d83cd9ce60da7b5a588422611
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-09-03 08:46:41 +0200

    add NODE_ENV=production support

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /**
- * Current url
+ * Set URL for api server
  */
+
+let appUrl = `http://localhost:${port}`; // local dev url
+
+// default prod url:
+if(process.env?.NODE_ENV === 'production'){
+  appUrl = 'https://screenreadthis-api-server.mandrasch';
+  console.log(`Starting with production mode on ${appUrl}`);
+}
+
+// for ploi
+// TODO: how can we setup an URL via process.env on ploi.io?
+
+// for render.com
 // https://render.com/docs/environment-variables
-const appUrl = process.env.RENDER_EXTERNAL_URL || `http://localhost:${port}`;
+  // const appUrl = process.env.RENDER_EXTERNAL_URL || `http://localhost:${port}`;
+if(process.env.RENDER_EXTERNAL_URL !== undefined){
+  console.log(`Detected render.com env, URL: ${appUrl}`);
+  appUrl = process.env.RENDER_EXTERNAL_URL;
+}
+
 
 /**
  * index page HTML
@@ -19,7 +37,7 @@ const html = `
 <!DOCTYPE html>
 <html lang="en-US">
   <head>
-    <title>Hello from Accessibility Tree API endpoint!</title>
+    <title>ScreenreadThis! API server endpoint</title>
     <style>
         body{
             font-family:Arial;
@@ -29,32 +47,8 @@ const html = `
   <body>
     <section>
       <h1>Hello from Accessibility Tree API endpoint demo!</h1>
+      <p>README: <a href="https://github.com/mandrasch/screenthis-api-server">https://github.com/mandrasch/screenreadthis-api-server</a></p>
 
-      <p><span style="font-style:italique;">Experimental.</span> This runs on a free tier on render.com, can be offline if limit is reached.</p>
-
-      <h2>API usage</h2>
-
-      <p>Use API via GET: <pre>${appUrl}/getA11yTree?url=ENCODED_URL</pre></p>
-
-      <p>Example: <pre>${appUrl}/getA11yTree?url=https%3A%2F%2Fwww.w3.org%2FWAI%2Fdemos%2Fbad%2Fbefore%2Fhome.html</pre></p>
-
-      <a href="${appUrl}/getA11yTree?url=https%3A%2F%2Fwww.w3.org%2FWAI%2Fdemos%2Fbad%2Fbefore%2Fhome.html">Open example response</a>
-
-      <p><small>Example site used: <a href="https://www.w3.org/WAI/demos/bad/before/home.html" target="_blank">WAI inaccessible homepage example</a> from <a href="https://www.w3.org/WAI/demos/bad/">WAI Demos before and after</a></small></p>
-
-      <h2>Get result for custom site</h2>
-      <form method="GET" action="${appUrl}/getA11yTree">
-        <label>URL: 
-            <input name="url" value="https://">
-        </label>
-        <p>The submitted URL will be transferred and logged on render.com. </p>
-        <button type="submit">GET</button>
-      </form>
-
-
-      <h2>About</h2>
-
-      <p>README: <a href="https://github.com/mandrasch/a11y-tree-api">https://github.com/mandrasch/a11y-tree-api</a></p>
     </section>
   </body>
 </html>
@@ -66,30 +60,41 @@ const puppeteer = require('puppeteer');
  * GET /getA11yTree endpoint
  * (cors enabled, https://expressjs.com/en/resources/middleware/cors.html)
  */
-app.get("/getA11yTree",cors(), async function (req, res) {
+app.get("/getA11yTree", cors(), async function (req, res) {
 
-    // Validation: check if url was submitted
-    if (!req.query.url) {
-        res.status(500).send({ error: 'No URL submitted' });
-        return;
+  // Validation: check if url was submitted
+  if (!req.query.url) {
+    res.status(500).send({ error: 'No URL submitted' });
+    return;
+  }
+
+  // TODO: sanitize url necessary?
+  const requestedUrl = req.query.url;
+  console.log(`GET /getA11yTree ...`);
+
+  // TODO: error catching?
+  console.log('Fire up puppeteer ...');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(requestedUrl, { waitUntil: 'domcontentloaded' });
+  const accessibilityTreeSnapshot = await page.accessibility.snapshot();
+
+  // get html lang
+  await page.evaluate(() => {
+    let htmlLang = document.querySelector('html').getAttribute("lang"); // returns null if not found
+    if (htmlLang === null) {
+      htmlLang = ''; // easier to parse
     }
+  })
 
-    // TODO: sanitize url necessary?
-    const requestedUrl = req.query.url;
-    console.log(`GET /getA11yTree ...`);
+  // clear up
+  await browser.close();
 
-    // TODO: error catching?
-    console.log('Fire up puppeteer ...');
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(requestedUrl, { waitUntil: 'domcontentloaded' });
-    const accessibilityTreeSnapshot = await page.accessibility.snapshot();
-    // clear up
-    await browser.close();
-
-    // send a11y tree as json
-    console.log('Successful! Sending json response...');
-    res.json(accessibilityTreeSnapshot);
+  // send a11y tree as json
+  console.log('Successful! Sending json response...');
+  let response = {... accessibilityTreeSnapshot};
+  response.lang = htmlLang;
+  res.json(response);
 })
 
 /**
@@ -101,6 +106,6 @@ app.get("/", (req, res) => res.type('html').send(html));
  * Start server
  */
 app.listen(port, () => {
-    console.log(`a11y-tree-api listening on port ${port}!`);
-    console.log(`Index page: ${appUrl}`);
+  console.log(`a11y-tree-api listening on port ${port}!`);
+  console.log(`Index page: ${appUrl}`);
 });
